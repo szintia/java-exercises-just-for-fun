c766e1d1b0ecccd834a7f9219a34789c0b35b1ea
szintia <kasziba.cintia@gmail.com>
2016-10-20 18:43:29 -0400

    modify the content of linked list

--- src/Exercise_02/Exercise_02_02/Exercise_2_2.java
+++ src/Exercise_02/Exercise_02_02/Exercise_2_2.java
@@ -12,7 +12,7 @@ public class Exercise_2_2 {
 	
 	public static void main(String... args) {
 		LinkedList list = new LinkedList();
-		list.add(4);
+		list.add(1);
 		list.add(4);
 		list.add(3);
 		list.add(4);
